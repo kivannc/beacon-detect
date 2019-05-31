f9e9b85986b345d640450e30ef7d5fe19121ecff
Kivanc ERTURK <erturkkivanc@gmail.com>
2019-05-31 15:41:01 +0300

    *

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ function sendData() {
         beaconsWithMean.beacons.push({
             id : key,
             rssi : Math.floor(rssiMean),
-            txPower : Math.floor(txPowerMean)
+            txPower : Math.floor(txPowerMean),
+            distance : calculateDistance(rssiMean)
         });
         //send with post in here
 	console.log(beaconsWithMean)
@@ -46,3 +47,23 @@ function sendData() {
 }
 
 var interval = setInterval(sendData,5000);
+
+
+
+function calculateDistance(rssi) {
+  
+    var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+    
+    if (rssi == 0) {
+      return -1.0; 
+    }
+  
+    var ratio = rssi*1.0/txPower;
+    if (ratio < 1.0) {
+      return Math.pow(ratio,10);
+    }
+    else {
+      var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+      return distance;
+    }
+  } 
